5a1e9301b640b4b5c833211082260b44a6ecbd38
Daryl Lin <daryllin@Daryls-MacBook-Pro.local>
2015-10-07 18:03:57 +0800

    Update DockerFile and Test files

--- test/ShopTest.php
+++ test/ShopTest.php
@@ -3,14 +3,21 @@
 include("../store.php");
 
 class ShopTest extends PHPUnit_Framework_TestCase {
-	public function testEmpty() {
-		$stack = array();
-        	$this->assertEmpty($stack);
-
-        	return $stack;
+	public function testFind() {
+		$product = "milk";
+		$store = new Store();
+		$this->assertNotEmpty($store->find($product));
 	}
 
-	public function testProductQuery() {
-		$this->assetNull
+	public function testFindParam() {
+		$product = 123;
+		$store = new Store();
+		$this->assertArrayHasKey('error_code', $store->find($product));
+	}
+
+	public function testAllProduct() {
+		$identify = "milk";
+		$store = new Store();
+		$this->assertArrayHasKey($identify,$store->all_product());
 	}
 }
